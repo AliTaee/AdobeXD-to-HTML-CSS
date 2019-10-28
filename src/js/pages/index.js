38580e9a7f13b6232d462b3a6cfea415b557370b
AliTaee <ali.taee.72@gmail.com>
2019-10-28 18:42:52 +0330

    Refactor js for submenu to improve performance

--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -5,27 +5,13 @@ $(document).ready(function () {
     // Hover sub menu
     $(".menu__sub, .menu__sub-menu").hover(function () {
         // Disable menu
-        $(".menu__sub-menu").css({
-            "opacity": "1",
-            "visibility": "visible"
-        });
+        $(".menu__sub-menu").addClass("menu__sub-menu--active");
         // Animation
-        $(".menu__arrow").css({
-            "transform": "rotate(-135deg)",
-            "-webkit-transform": "rotate(-135deg)",
-            "transition": "1s",
-        });
+        $(".menu__arrow").addClass("menu__arrow--active");
     }, function () {
         // Hide menu
-        $(".menu__sub-menu").css({
-            "opacity": "0",
-            "visibility": "hidden"
-        });
+        $(".menu__sub-menu").removeClass("menu__sub-menu--active");
         // Animation
-        $(".menu__arrow").css({
-            "transform": "rotate(45deg)",
-            "-webkit-transform": "rotate(45deg)",
-            "transition": "1s",
-        });
+        $(".menu__arrow").removeClass("menu__arrow--active");
     });
 });
