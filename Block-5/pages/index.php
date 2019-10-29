e8e00b9b5d2d6738742a5ae4f45b5f24ba0097aa
AliTaee <ali.taee.72@gmail.com>
2019-10-29 09:59:55 +0330

    Refactor style css

--- Block-5/pages/index.php
+++ Block-5/pages/index.php
@@ -19,62 +19,8 @@
 
 <body>
 	<div class="intro">
-		<header class="menu">
-			<div class="menu__wrapper">
-
-				<a href="#" class="menu__logo">
-					<img src="../src/images/site-logo.png" alt="site logo" class="menu__img-logo">
-					<div class="menu__text-wrapper">
-						<h1 class="menu__site-title">
-							رستوران ایکس
-						</h1>
-						<p class="menu__site-desc">طعم‌های ناشناخته</p>
-					</div>
-				</a>
-
-				<nav class="menu__nav">
-					<ul class="menu__list">
-						<li class="menu__item"><a href="#" class="menu__link">خانه</a></li>
-						<li class="menu__item">
-							<p class="menu__sub menu__link">محصولات<span class="menu__arrow menu__arrow--down"></span></p>
-							<ul class="menu__sub-menu">
-								<li class="menu__sub-item"><a class="menu__sub-link" href="#">ساب منو</a></li>
-								<li class="menu__sub-tem"><a class="menu__sub-link" href="#">ساب منو</a></li>
-								<li class="menu__sub-item"><a class="menu__sub-link" href="#">ساب منو</a></li>
-								<li class="menu__sub-item"><a class="menu__sub-link" href="#">ساب منو</a></li>
-							</ul>
-						</li>
-						<li class="menu__item"><a href="#" class="menu__link">درباره </a></li>
-						<li class="menu__item"><a href="#" class="menu__link">تماس</a></li>
-					</ul>
-				</nav>
-
-				<div class="menu__contact">
-					<p class="menu__tel">تلفن سفارشات</p>
-					<a href="tel:+9821750440" class="menu__tel-number"><span class="menu__area-number">۰۲۱</span>۷۵۰
-						۴۴۰</a>
-				</div>
-
-			</div>
-		</header>
-		<section class="slider">
-			<div class="slider__wrapper">
-				<div class="slider__item slider__item--active" style="background-image: -webkit-gradient(linear, left top, left bottom, from(rgba(0, 0, 0, 0.201))), url('../src/images/img1.png')">
-					<h2 class="slider__title">طعم‌های ناشناخته را با ما تجربه کنید</h2>
-					<p class="slider__desc">۱۵ سال همراه‌تان بوده‌ایم</p>
-				</div>
-				<div class="slider__item" style="background-image: -webkit-gradient(linear, left top, left bottom, from(rgba(0, 0, 0, 0.201))), url('../src/images/img2.jpg')">
-					<h2 class="slider__title">طعم‌های ناشناخته را با ما تجربه کنید</h2>
-					<p class="slider__desc">۱۵ سال همراه‌تان بوده‌ایم</p>
-				</div>
-				<div class="slider__item" style="background-image: -webkit-gradient(linear, left top, left bottom, from(rgba(0, 0, 0, 0.201))), url('../src/images/img3.jpg')">
-					<h2 class="slider__title">طعم‌های ناشناخته را با ما تجربه کنید</h2>
-					<p class="slider__desc">۱۵ سال همراه‌تان بوده‌ایم</p>
-				</div>
-				<div class="slider__nav">
-				</div>
-			</div>
-		</section>
+		<?php include 'common/header.php'; ?>
+		<?php include 'block/index/slider.php'; ?>
 	</div>
 
 	<?php include 'common/js.php'; ?>
--- Block-5/src/js/pages/index.js
+++ Block-5/src/js/pages/index.js
@@ -5,34 +5,34 @@ let index = 1;
 
 $(document).ready(function () {
 
-    // Hover sub menu
-    $(".menu__sub, .menu__sub-menu").hover(function () {
-        // Disable menu
-        $(".menu__sub-menu").addClass("menu__sub-menu--active");
-        // Animation
-        $(".menu__arrow").addClass("menu__arrow--active");
-    }, function () {
-        // Hide menu
-        $(".menu__sub-menu").removeClass("menu__sub-menu--active");
-        // Animation
-        $(".menu__arrow").removeClass("menu__arrow--active");
-    });
+	// Hover sub menu
+	$(".menu__sub, .menu__sub-menu").hover(function () {
+		// Disable menu
+		$(".menu__sub-menu").addClass("menu__sub-menu--active");
+		// Animation
+		$(".menu__arrow").addClass("menu__arrow--active");
+	}, function () {
+		// Hide menu
+		$(".menu__sub-menu").removeClass("menu__sub-menu--active");
+		// Animation
+		$(".menu__arrow").removeClass("menu__arrow--active");
+	});
 
-    function sliderFunc() {
-        let slider = $(".slider__item");
+	function sliderFunc() {
+		let slider = $(".slider__item");
 
-        if (index >= slider.length) {
-            $(slider[index - 1]).removeClass("slider__item--active").fadeOut(1000);
-            index = 0;
-            $(slider[index]).addClass("slider__item--active").fadeIn(1000);
-        } else {
-            $(slider[index - 1]).removeClass("slider__item--active").fadeOut(1000);
-            $(slider[index]).addClass("slider__item--active").fadeIn(1000);
-            index++;
-        }
+		if (index >= slider.length) {
+			$(slider[index - 1]).removeClass("slider__item--active").fadeOut(1000);
+			index = 0;
+			$(slider[index]).addClass("slider__item--active").fadeIn(1000);
+		} else {
+			$(slider[index - 1]).removeClass("slider__item--active").fadeOut(1000);
+			$(slider[index]).addClass("slider__item--active").fadeIn(1000);
+			index++;
+		}
 
-        setTimeout(sliderFunc, TIME);
-    }
+		setTimeout(sliderFunc, TIME);
+	}
 
-    window.onload = sliderFunc;
+	window.onload = sliderFunc;
 });
